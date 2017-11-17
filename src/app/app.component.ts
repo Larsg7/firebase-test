c15c6fe952b1c35f58a6dd219e576854ba4bdc21
Lars Gröber <larsgroeber7@gmail.com>
2017-11-17 18:24:45 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface Idea {
+  id: string;
+  name: string;
+  idea: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  private ideaColl: AngularFirestoreCollection<Idea>;
+  ideas: Observable<Idea[]>;
+
+  name: string;
+
+  formGroup: FormGroup;
+
+  constructor(private db: AngularFirestore, private formBuilder: FormBuilder) {
+    this.ideaColl = db.collection<Idea>('Ideas', ref => {
+      return ref;
+    });
+    this.ideas = this.ideaColl.valueChanges();
+    this.buildForm();
+  }
+
+  private buildForm(idea?: Idea) {
+    this.formGroup = this.formBuilder.group({
+      id: [idea ? idea.id : ''],
+      name: [idea ? idea.name : '', Validators.required],
+      idea: [idea ? idea.idea : '', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.ideas.subscribe(res => console.log(res));
+  }
+
+  save() {
+    if (this.formGroup.invalid) return;
+
+    console.log(this.formGroup);
+
+    const idea: Idea = {
+      id: this.formGroup.get('id').value,
+      name: this.formGroup.get('name').value,
+      idea: this.formGroup.get('idea').value,
+    };
+
+    if (idea.id) {
+      this.getDocById(idea.id)
+        .then(res => {
+          res.forEach(doc => doc.ref.update(idea));
+          this.formGroup.reset();
+        });
+      return;
+    }
+
+    this.ideaColl.add({
+      id: this.db.createId(),
+      name: idea.name,
+      idea: idea.idea,
+    }).then(() => {
+      this.formGroup.reset();
+    });
+  }
+
+  update(idea: Idea) {
+    if (!idea) return;
+
+    this.buildForm(idea);
+  }
+
+  deleteIdea(idea: Idea) {
+    if (!idea) return;
+    this.getDocById(idea.id)
+      .then(res => {
+        res.forEach(doc => doc.ref.delete());
+      });
+  }
+
+  private getDocById(id: string) {
+    return this.ideaColl.ref.where('id', '==', id).get();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import {
+  MatButtonModule, MatCardModule, MatCheckboxModule, MatIconModule, MatInputModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
